9c353ff6c7610a7352caa33bfecc0804c2ea08cb
MelkdeSousa <melk.sousap@gmail.com>
2021-02-13 00:38:57 -0300

    floating-point values and notation to exponentiation

---
+++ basic/types/floats.ex
@@ -0,0 +1,5 @@
+# Floating point numbers require a decimal after at least one digit.
+# They have 64-bit double precision and support 'e' for exponent values:
+
+IO.puts(3.141592653589)
+IO.puts(6.67e-11)
