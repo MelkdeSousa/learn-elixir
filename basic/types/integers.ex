8ee55f9b829e1ecdfb74be87db495964cf7e528d
MelkdeSousa <melk.sousap@gmail.com>
2021-02-13 00:34:58 -0300

    integer values in decimal and other bases

---
+++ basic/types/integers.ex
@@ -0,0 +1,13 @@
+# is a integer
+integer = 255
+# is a binary
+binary = 0b0110
+# is a octal
+octal = 0o644
+# is a hexdecimal
+hex = 0x1FA
+
+IO.puts(integer)
+IO.puts(binary)
+IO.puts(octal)
+IO.puts(hex)
